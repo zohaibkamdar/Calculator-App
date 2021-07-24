230c12cd4974c27b1b408aa6ba4f09e45b9588ee
zohaibkamdar <87900142+zohaibkamdar@users.noreply.github.com>
2021-07-24 16:58:00 +0500

    Update CALapp.js

--- CALapp.js
+++ CALapp.js
@@ -2,15 +2,15 @@ function dis(val)
 {
 document.getElementById("cal").value+=val
 &nbsp;}
-//function for evaluation
+
 function solve()
 {
 let x = document.getElementById("cal").value
 let y = eval(x)
 document.getElementById("cal").value = y
 }
-//function for clearing the display
+
 function clr()
 {
-document.getElementById("cal").value = "void"
+document.getElementById("cal").value = ""
 }
